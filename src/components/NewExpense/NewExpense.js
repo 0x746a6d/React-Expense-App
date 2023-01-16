0a59d67718c551d1742fcae81d226340f82b49ac
0x746a6d <ganymedenine16368@gmail.com>
2023-01-16 16:20:24 -0500

    added form adjustments so it pops in and out based on button clicks. added another year for 2023 listing

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import React, { useState, useTransition } from "react";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      {/* Insert form component in here */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}  //passed as a vale to the onCancel prop in expenseForm component
+        />
+      )}
     </div>
   );
 };
